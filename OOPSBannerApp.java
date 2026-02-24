458f86348254b8840eff90073ab2ac8de56d28b7
aku2028 <akkuaccu1617@gmail.com>
2026-02-24 14:20:28 +0530

    UC2: ASCII OOPS Banner

---
+++ OOPSBannerApp.java
@@ -0,0 +1,17 @@
+/**
+ * OOPS Banner App - UC1
+ * Prints the text "OOPS" to the console.
+ *
+ * @author Akkuu
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
---
+++ OOPSBannerApp2.java
@@ -0,0 +1,20 @@
+/**
+ * OOPS Banner App - UC2
+ * Prints "OOPS" as ASCII art using a 7-line grid.
+ *
+ * @author Akkuu
+ * @version 2.0
+ */
+public class OOPSBannerApp2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" ***   ***   *****   **** ");
+        System.out.println("*   * *   *  *    * *    *");
+        System.out.println("*   * *   *  *    * *     ");
+        System.out.println("*   * *   *  *****   **** ");
+        System.out.println("*   * *   *  *            *");
+        System.out.println("*   * *   *  *       *    *");
+        System.out.println(" ***   ***   *        **** ");
+    }
+}
